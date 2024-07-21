d9984c445b545ba0894cf04e346da3eaf01b2b34
Suresh Sala <you@example.com>
2024-07-21 14:57:37 +0530

    misc

--- app/src/main/java/com/example/myapplication/wifidirect/WifiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/example/myapplication/wifidirect/WifiDirectBroadcastReceiver.kt
@@ -23,20 +23,20 @@ class WifiDirectBroadcastReceiver(
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        var action: String? = intent?.action
-        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION == action) {
-            var state: Int = intent!!.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
-            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                Toast.makeText(context, "Wifi is ON", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(context, "Wifi is OFF", Toast.LENGTH_SHORT).show()
-            }
-        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION == action) {
-            wifiP2pManager.requestPeers(channel, activity.peerListListener)
-        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION == action) {
-
-        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION == action) {
-
-        }
+//        var action: String? = intent?.action
+//        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION == action) {
+//            var state: Int = intent!!.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+//            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+//                Toast.makeText(context, "Wifi is ON", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(context, "Wifi is OFF", Toast.LENGTH_SHORT).show()
+//            }
+//        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION == action) {
+//            wifiP2pManager.requestPeers(channel, activity.peerListListener)
+//        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION == action) {
+//
+//        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION == action) {
+//
+//        }
     }
 }
